4714cba9089120839e3cba19a4ec5585c2e794f7
Version <ivan.mikityuk4@gmail.com>
2023-08-01 12:22:28 +0300

    add archive table

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 const notesData = [
     {
         id: 1,
+        name: "Watch a movie",
+        created: "2023-07-12 17:22",
+        content: "Fight club",
+        category: "Random Thought",
+        dates: [""]
+    },
+    {
+        id: 2,
+        name: "Buy a car",
+        created: "2023-07-27 22:12",
+        content: "BMW M3",
+        category: "Idea",
+        dates: [""]
+    },
+    {
+        id: 3,
         name: "New Feature",
         created: "2023-08-01 10:00",
         content: "I’m gonna have a dentist appointment on the 3/5/2021, I moved it from 5/5/2021",
@@ -8,12 +24,36 @@ const notesData = [
         dates: ["2023-08-05, 2023-08-07"]
     },
     {
-        id: 2,
+        id: 4,
         name: "Shopping list",
         created: "2023-08-01 10:58",
         content: "Tomatoes, bread",
         category: "Task",
         dates: [""]
+    },
+    {
+        id: 5,
+        name: "Write a book",
+        created: "2023-08-01 11:03",
+        content: "Write a book in the adventure genre",
+        category: "Idea",
+        dates: [""]
+    },
+    {
+        id: 6,
+        name: "Take a walk in the park",
+        created: "2023-08-01 11:22",
+        content: "Go to Kyoto Park",
+        category: "Random Thought",
+        dates: [""]
+    },
+    {
+        id: 7,
+        name: "Birthday",
+        created: "2023-08-01 11:40",
+        content: "Buy a gift and order a restaurant",
+        category: "Task",
+        dates: ["2023-08-22"]
     },
 ];
 
@@ -77,40 +117,63 @@ function updateTables() {
   updateSummaryTable();
 }
 
-// Функція для архівації нотатки
+// Функція для архівації або розархівації нотатки
 function archiveNote(noteId) {
-  for (var i = 0; i < notesData.length; i++) {
-    if (notesData[i].id === noteId) {
-      notesData[i].archived = true;
-      updateTables();
-      return;
+    for (let i = 0; i < notesData.length; i++) {
+        if (notesData[i].id === noteId) {
+            notesData[i].archived = !notesData[i].archived;
+            updateTables();
+            return;
+        }
     }
-  }
+}
+
+function toggleArchivedTable() {
+    const archivedTable = document.getElementById("archived-table");
+    const toggleArchivedTableBtn = document.getElementById("toggleArchivedTableBtn");
+
+    if (archivedTable.style.display === "none") {
+        archivedTable.style.display = "table";
+        toggleArchivedTableBtn.textContent = "Hide Archive";
+    } else {
+        archivedTable.style.display = "none";
+        toggleArchivedTableBtn.textContent = "Show Archive";
+    }
+}
+
+const createNoteMenu = document.getElementById("hidden_note_menu");
+
+function openCreateNoteMenu() {
+    createNoteMenu.style.display = "flex";
+}
+
+function closeCreateNoteMenu() {
+    createNoteMenu.style.display = "none";
 }
 
 // Функція для редагування нотатки
 function editNote(noteId) {
-  for (var i = 0; i < notesData.length; i++) {
-    if (notesData[i].id === noteId) {
-      var newContent = prompt("Enter new content for the note:", notesData[i].content);
+    for (let i = 0; i < notesData.length; i++) {
+        if (notesData[i].id === noteId) {
+        const newContent = prompt("Enter new content for the note:", notesData[i].content);
 
-      if (newContent !== null && newContent.trim() !== "") {
-        notesData[i].content = newContent.trim();
-        notesData[i].datesMentioned = newContent.match(/\d{1,2}\/\d{1,2}\/\d{4}/g) || [];
-        updateTables();
-      }
-      return;
+            if (newContent !== null && newContent.trim() !== "") {
+                notesData[i].content = newContent.trim();
+                notesData[i].datesMentioned = newContent.match(/\d{1,2}\/\d{1,2}\/\d{4}/g) || [];
+                updateTables();
+            }
+            return;
+        }
     }
-  }
 }
 
 // Функція для видалення нотатки
 function deleteNote(noteId) {
-  for (var i = 0; i < notesData.length; i++) {
-    if (notesData[i].id === noteId) {
-      notesData.splice(i, 1);
-      updateTables();
-      return;
+    for (let i = 0; i < notesData.length; i++) {
+        if (notesData[i].id === noteId) {
+        notesData.splice(i, 1);
+        updateTables();
+        return;
     }
   }
 }
@@ -147,55 +210,54 @@ function addNote() {
 
 // Функція для оновлення таблиці зі зведеною інформацією
 function updateSummaryTable() {
-  var summaryTable = document.getElementById("summary-table");
-  var categories = ["Task", "Random Thought", "Idea"];
+    const summaryTable = document.getElementById("summary-table");
+    const categories = ["Task", "Random Thought", "Idea"];
 
-  // Очищення таблиці
-  summaryTable.innerHTML = `
-    <tr>
-      <th>Category</th>
-      <th>Active Notes Count</th>
-      <th>Archived Notes Count</th>
-    </tr>
-  `;
+    // Очищення таблиці
+    summaryTable.innerHTML = ` 
+        <tr>
+            <th>Category</th>
+            <th>Active</th>
+            <th>Archived</th>
+        </tr>
+    `;
 
-  // Отримання кількості активних та архівованих нотаток за категоріями
-  var summaryData = [];
-  for (var i = 0; i < categories.length; i++) {
-    var category = categories[i];
-    var activeNotesCount = 0;
-    var archivedNotesCount = 0;
+    // Отримання кількості активних та архівованих нотаток за категоріями
+    let summaryData = [];
+    for (let i = 0; i < categories.length; i++) {
+        let category = categories[i];
+        let activeNotesCount = 0;
+        let archivedNotesCount = 0;
 
-    for (var j = 0; j < notesData.length; j++) {
-      var note = notesData[j];
-      if (note.category === category) {
-        if (note.archived) {
-          archivedNotesCount++;
-        } else {
-          activeNotesCount++;
+        for (let j = 0; j < notesData.length; j++) {
+            let note = notesData[j];
+            if (note.category === category) {
+                if (note.archived) {
+                    archivedNotesCount++;
+                } else {
+                    activeNotesCount++;
+                }
+            }
         }
-      }
+
+        summaryData.push({
+            category: category,
+            activeNotesCount: activeNotesCount,
+            archivedNotesCount: archivedNotesCount
+        });
     }
 
-    summaryData.push({
-      category: category,
-      activeNotesCount: activeNotesCount,
-      archivedNotesCount: archivedNotesCount
-    });
-  }
-
-  // Заповнення таблиці зі зведеною інформацією
-  for (var i = 0; i < summaryData.length; i++) {
-    var summary = summaryData[i];
-    var tableRow = document.createElement("tr");
-    tableRow.innerHTML = `
-      <td>${summary.category}</td>
-      <td>${summary.activeNotesCount}</td>
-      <td>${summary.archivedNotesCount}</td>
-    `;
-    summaryTable.appendChild(tableRow);
-  }
+    // Заповнення таблиці зі зведеною інформацією
+    for (let i = 0; i < summaryData.length; i++) {
+        const summary = summaryData[i];
+        const tableRow = document.createElement("tr");
+        tableRow.innerHTML = `
+            <td>${summary.category}</td>
+            <td>${summary.activeNotesCount}</td>
+            <td>${summary.archivedNotesCount}</td>
+        `;
+        summaryTable.appendChild(tableRow);
+    }
 }
 
-// Початкове оновлення таблиць при завантаженні сторінки
 updateTables();
